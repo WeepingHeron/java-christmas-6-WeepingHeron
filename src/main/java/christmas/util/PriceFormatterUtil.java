5dcda9fec6c6f7a877f4aadabdfb0f385bab8e39
WeepingHeron <kimtaehoon_98@naver.com>
2023-11-09 22:25:31 +0900

    refactor: separated from Calculator.java

---
+++ src/main/java/christmas/util/PriceFormatterUtil.java
@@ -0,0 +1,11 @@
+package christmas.util;
+
+import java.text.DecimalFormat;
+
+public class PriceFormatterUtil {
+
+    public String getFormattedPrice(Integer price) {
+        DecimalFormat formatter = new DecimalFormat("###,###원");
+        return formatter.format(price);
+    }
+}
---
+++ src/test/java/christmas/util/PriceFormatterUtilTest.java
@@ -0,0 +1,24 @@
+package christmas.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PriceFormatterUtilTest {
+
+    @DisplayName("인자로 들어온 price를 포맷에 맞추어 반환한다.")
+    @Test
+    void getFormattedPrice() {
+        // Given
+        Integer price = 142000;
+
+        // When
+        PriceFormatterUtil util = new PriceFormatterUtil();
+        String formattedPrice = util.getFormattedPrice(price);
+
+        // Then
+        Assertions.assertEquals("142,000원", formattedPrice);
+    }
+}
