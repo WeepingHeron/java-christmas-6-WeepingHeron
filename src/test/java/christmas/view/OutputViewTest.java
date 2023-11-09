6926b098d4252845f175fe993e7bf7fae123b363
WeepingHeron <kimtaehoon_98@naver.com>
2023-11-09 22:28:50 +0900

    test: added printMenuTest

--- src/test/java/christmas/view/OutputViewTest.java
+++ src/test/java/christmas/view/OutputViewTest.java
@@ -10,33 +10,27 @@ import java.util.Map;
 class OutputViewTest {
 
     private OutputView outputView;
+    private Map<String, Integer> order = new LinkedHashMap<>();
 
     @BeforeEach
     void setup() {
         outputView = new OutputView();
+        order.put("티본스테이크", 1);
+        order.put("바비큐립", 1);
+        order.put("초코케이크", 2);
+        order.put("제로콜라", 1);
     }
 
     @DisplayName("Map 형태로 받은 주문을 출력한다.")
     @Test
     void printMenuTest() {
-        // Given
-        Map<String, Integer> order = new LinkedHashMap<>();
-        order.put("티본스테이크", 1);
-        order.put("바비큐립", 1);
-        order.put("초코케이크", 2);
-        order.put("제로콜라", 1);
-
-        // When & Then
         outputView.printMenu(order);
     }
 
     @DisplayName("할인 전 총주문 금액을 출력한다.")
     @Test
     void printListPriceTest() {
-        // Given
-        Integer testPrice = 142000;
-
-        // When & Then
-        outputView.printListPrice(testPrice);
+        Integer price = 142000;
+        outputView.printListPrice(price);
     }
 }
