db44e363b4ae524324574859036ccebbfacee66a
WeepingHeron <kimtaehoon_98@naver.com>
2023-11-09 22:30:40 +0900

    feat: added PromotionController.java

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,20 @@
 package christmas;
 
+import christmas.controller.PromotionController;
+import christmas.domain.PromotionModel;
+import christmas.domain.calculate.Calculator;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        PromotionModel model = new PromotionModel();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Calculator calculator = new Calculator();
+
+        PromotionController promotionController = new PromotionController(model, inputView, outputView, calculator);
+
+        promotionController.updateView();
     }
 }
