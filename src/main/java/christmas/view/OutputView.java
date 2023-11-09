5b7741d6b77382cf0da350f67f80858cf3bdc16a
WeepingHeron <kimtaehoon_98@naver.com>
2023-11-09 22:29:24 +0900

    feat: imports and uses PriceFormatterUtil

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,9 +1,17 @@
 package christmas.view;
 
-import java.text.DecimalFormat;
+import christmas.util.PriceFormatterUtil;
+
 import java.util.Map;
 
 public class OutputView {
+
+    PriceFormatterUtil format = new PriceFormatterUtil();
+
+    public void printDate(Integer date) {
+        System.out.println("12월 " + date + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        System.out.println();
+    }
 
     public void printMenu(Map<String, Integer> order) {
         System.out.println("<주문 메뉴>");
@@ -14,15 +22,11 @@ public class OutputView {
 
             System.out.println(menu + " " + quantity + "개");
         }
+        System.out.println();
     }
 
-    public void printListPrice(Integer calculatedListPrice) {
+    public void printListPrice(Integer addedPrice) {
         System.out.println("<할인 전 총주문 금액>");
-        System.out.println(getFormattedPrice(calculatedListPrice));
-    }
-
-    private String getFormattedPrice(Integer calculatedListPrice) {
-        DecimalFormat formatter = new DecimalFormat("###,###원");
-        return formatter.format(calculatedListPrice);
+        System.out.println(format.getFormattedPrice(addedPrice));
     }
 }
