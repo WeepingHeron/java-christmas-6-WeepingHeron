30215a59f911b67ec65b2ec42ef68cc80c0dbed3
WeepingHeron <kimtaehoon_98@naver.com>
2023-11-10 16:53:29 +0900

    feat: added Applier

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -2,6 +2,7 @@ package christmas;
 
 import christmas.controller.PromotionController;
 import christmas.domain.PromotionModel;
+import christmas.domain.events.Applier;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
@@ -10,8 +11,9 @@ public class Application {
         PromotionModel model = new PromotionModel();
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
+        Applier applier = new Applier();
 
-        PromotionController promotionController = new PromotionController(model, inputView, outputView);
+        PromotionController promotionController = new PromotionController(model, inputView, outputView, applier);
 
         promotionController.updateView();
     }
