51e884a7be0f388cc2a1f728bb3b8b1747614758
WeepingHeron <kimtaehoon_98@naver.com>
2023-11-10 16:42:16 +0900

    feat: discounts by d-day

---
+++ src/main/java/christmas/domain/events/discounts/ChristmasDiscount.java
@@ -0,0 +1,29 @@
+package christmas.domain.events.discounts;
+
+public class ChristmasDiscount {
+
+    public Integer applyChristmasDiscount(Integer date) {
+
+        int discountedAmount = 0;
+        if (checkChristmasDiscount(date)) {
+            discountedAmount = calculateChristmasDiscount(date);
+        }
+        return discountedAmount;
+    }
+
+    private boolean checkChristmasDiscount(Integer date) {
+        if (1 <= date && date <= 25) {
+            return true;
+        }
+        return false;
+    }
+
+    private int calculateChristmasDiscount(Integer date) {
+        int initialDate = 1;
+        int initialAmount = 1000;
+        int daysPassed = date - initialDate;
+        int discountAmount = initialAmount + (daysPassed * 100);
+
+        return discountAmount;
+    }
+}
