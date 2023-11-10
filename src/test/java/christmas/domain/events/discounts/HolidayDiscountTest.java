5929937e6195d04290485e3f96d6b7b5b5e0817c
WeepingHeron <kimtaehoon_98@naver.com>
2023-11-10 16:44:25 +0900

    test: tests HolidayDiscount.java

---
+++ src/test/java/christmas/domain/events/discounts/HolidayDiscountTest.java
@@ -0,0 +1,25 @@
+package christmas.domain.events.discounts;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class HolidayDiscountTest {
+
+    @DisplayName("공휴일의 경우 특별 할인을 진행한다.")
+    @Test
+    void applyHolidayDiscountTest() {
+        // Given
+        Integer notHoliday = 2;
+        Integer holiday = 3;
+        HolidayDiscount holidayDiscount = new HolidayDiscount();
+
+        // When
+        Integer result1 = holidayDiscount.applyHolidayDiscount(notHoliday);
+        Integer result2 = holidayDiscount.applyHolidayDiscount(holiday);
+
+        // Then
+        Assertions.assertEquals(0, result1);
+        Assertions.assertEquals(1000, result2);
+    }
+}
