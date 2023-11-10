11c3f4867388fcba83513c892d5b754e0abfa037
WeepingHeron <kimtaehoon_98@naver.com>
2023-11-10 16:53:11 +0900

    feat: added codes about applier

--- src/main/java/christmas/controller/PromotionController.java
+++ src/main/java/christmas/controller/PromotionController.java
@@ -1,23 +1,34 @@
 package christmas.controller;
 
 import christmas.domain.PromotionModel;
+import christmas.domain.events.Applier;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
 public class PromotionController {
+
     private PromotionModel model;
     private InputView inputView;
     private OutputView outputView;
+    private Applier applier;
 
-    public PromotionController(PromotionModel model, InputView inputView, OutputView outputView) {
+    public PromotionController(PromotionModel model, InputView inputView, OutputView outputView, Applier applier) {
         this.model = model;
         this.inputView = inputView;
         this.outputView = outputView;
+        this.applier = applier;
     }
 
     public void updateView() {
         runInputView();
         model.setAddedPrice(model.getOrder());
+        applier.applyDiscounts(model.getAddedPrice(), model.getDate(), model.getOrder());
+        applier.applyEvents(model.getAddedPrice(), model.getDate(), model.getOrder());
+        System.out.println(model.getAddedPrice());
+        System.out.println(model.getAppliedEvents());
+        System.out.println(model.getDiscountedAmount());
+        System.out.println(model.getOrder());
+        System.out.println(model.getDate());
         runOutputView();
     }
 
@@ -30,5 +41,10 @@ public class PromotionController {
         outputView.printDate(model.getDate());
         outputView.printMenu(model.getOrder());
         outputView.printAddedPrice(model.getAddedPrice());
+        outputView.printGift(model.getAppliedEvents());
+        outputView.printAppliedEvents(model.getAppliedEvents());
+        outputView.printDiscountedAmount(model.getDiscountedAmount());
+        outputView.printFinalPrice(model.getAddedPrice(), model.getDiscountedAmount());
+        outputView.printBadge(model.getDiscountedAmount());
     }
 }
