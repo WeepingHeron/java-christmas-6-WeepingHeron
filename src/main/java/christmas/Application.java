a2b3b8c574d50e47e115e207ff30e97b4142f1f0
WeepingHeron <kimtaehoon_98@naver.com>
2023-11-11 15:10:10 +0900

    feat: added Validator

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -3,17 +3,20 @@ package christmas;
 import christmas.controller.PromotionController;
 import christmas.domain.PromotionModel;
 import christmas.domain.events.Applier;
+import christmas.validator.Validator;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
         PromotionModel model = new PromotionModel();
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
         Applier applier = new Applier();
+        Validator validator = new Validator();
 
-        PromotionController promotionController = new PromotionController(model, inputView, outputView, applier);
+        PromotionController promotionController = new PromotionController(model, inputView, outputView, applier, validator);
 
         promotionController.updateView();
     }
