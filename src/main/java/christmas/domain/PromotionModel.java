420472bdd8ee4f9067478b30d4b19491f7ab415c
WeepingHeron <kimtaehoon_98@naver.com>
2023-11-14 12:10:22 +0900

    refactor(PromotionModel): reduced instance variables and related methods

--- src/main/java/christmas/domain/PromotionModel.java
+++ src/main/java/christmas/domain/PromotionModel.java
@@ -1,20 +1,13 @@
 package christmas.domain;
 
-import christmas.domain.calculate.Calculator;
-
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class PromotionModel {
 
-    Calculator calculator = new Calculator();
-
     private Integer date;
     private Map<String, Integer> order = new HashMap<>();
-    private Integer addedPrice = 0;
-    private Map<String, Integer> appliedEvents = new HashMap<>();
-    private Integer discountedAmount = 0;
 
     public Integer getDate() {
         return date;
@@ -24,35 +17,11 @@ public class PromotionModel {
         this.date = date;
     }
 
-    public Integer getAddedPrice() {
-        return addedPrice;
-    }
-
-    public void setAddedPrice(Map<String, Integer> order) {
-        addedPrice = calculator.addPrice(order);
-    }
-
     public Map<String, Integer> getOrder() {
         return Collections.unmodifiableMap(order);
     }
 
     public void setOrder(Map<String, Integer> order) {
         this.order = order;
-    }
-
-    public Map<String, Integer> getAppliedEvents() {
-        return Collections.unmodifiableMap(appliedEvents);
-    }
-
-    public void setAppliedEvents(String key, Integer value) {
-        appliedEvents.put(key, value);
-    }
-
-    public Integer getDiscountedAmount() {
-        return discountedAmount;
-    }
-
-    public void setDiscountedAmount(Integer discountedAmount) {
-        this.discountedAmount = discountedAmount;
     }
 }
