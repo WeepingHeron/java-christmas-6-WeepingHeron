3ec8aef69786b4f07eb0f623380de73e32453aae
WeepingHeron <kimtaehoon_98@naver.com>
2023-11-14 13:20:35 +0900

    refactor(Gift): added private static final variables

--- src/main/java/christmas/domain/events/Gift.java
+++ src/main/java/christmas/domain/events/Gift.java
@@ -2,19 +2,19 @@ package christmas.domain.events;
 
 public class Gift {
 
+    private static final int GIFT_EVENT_THRESHOLD = 120000;
+    private static final int GIFT_DISCOUNT_AMOUNT = 25000;
+
     public Integer applyGiftEvent(Integer addedPrice) {
         int discountedAmount = 0;
 
         if (checkGiftEvent(addedPrice)) {
-            discountedAmount += 25000;
+            discountedAmount += GIFT_DISCOUNT_AMOUNT;
         }
         return discountedAmount;
     }
 
     private boolean checkGiftEvent(Integer addedPrice) {
-        if (addedPrice >= 120000) {
-            return true;
-        }
-        return false;
+        return addedPrice >= GIFT_EVENT_THRESHOLD;
     }
 }
